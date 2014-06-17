3209c3baec3f3435f983170f758e4e4a72b06d8e
Manuel Quiñones <manuel.por.aca@gmail.com>
2014-06-17 10:11:57 -0300

    Add example for sessions

---
+++ misc/test_sessions.sql
@@ -0,0 +1,55 @@
+insert into laptops values ('8bfe93bb895fa79fd0e59550af8c97ff07863666', '110E8400-E29B-11D4-A716-446655440000',
+'XO-1.5', '20140115', null, null, null, null);
+insert into laptops values ('95fa79ff07863666d0e59550af88bfe93bb8c97f', '22EA7340-AA43-1D2D-AAAA-242342344444',
+'XO-1.75', '20140115', null, null, null, null);
+insert into laptops values ('fffe93bb07863666d0e59550af88b8c97f95fa79', '33879BAA-1234-9898-ABBA-123148726500',
+'XO-1.75', '20140115', null, null, null, null);
+insert into laptops values ('7f95fa79ff07863666d0e59550af88bfe93bb8c9', '443424CA-4889-8588-AC12-999999999999',
+'XO-4', '20140115', null, null, null, null);
+
+insert into learners values ('8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+insert into learners values ('95fa79ff07863666d0e59550af88bfe93bb8c97f', 0, '');
+insert into learners values ('fffe93bb07863666d0e59550af88b8c97f95fa79', 0, '');
+insert into learners values ('7f95fa79ff07863666d0e59550af88bfe93bb8c9', 0, '');
+
+insert into sessions values (1401607800, 8100, 1, '8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+insert into sessions values (1401617100, 15000, 1, '8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+insert into sessions values (1401698100, 12420, 1, '8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+insert into sessions values (1401728400, 960, 0, '8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+insert into sessions values (1401868560, 18660, 1, '8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+insert into sessions values (1402069320, 12060, 1, '8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+insert into sessions values (1402084500, 6300, 0, '8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+
+insert into sessions values (1401607800, 8100, 1, '95fa79ff07863666d0e59550af88bfe93bb8c97f', 0, '');
+insert into sessions values (1401739920, 9420, 0, '95fa79ff07863666d0e59550af88bfe93bb8c97f', 0, '');
+insert into sessions values (1401820980, 19260, 0, '95fa79ff07863666d0e59550af88bfe93bb8c97f', 0, '');
+insert into sessions values (1401908340, 4440, 0, '95fa79ff07863666d0e59550af88bfe93bb8c97f', 0, '');
+insert into sessions values (1402082580, 4560, 0, '95fa79ff07863666d0e59550af88bfe93bb8c97f', 0, '');
+insert into sessions values (1402088640, 7320, 0, '95fa79ff07863666d0e59550af88bfe93bb8c97f', 0, '');
+
+insert into sessions values (1401876240, 6900, 1, 'fffe93bb07863666d0e59550af88b8c97f95fa79', 0, '');
+insert into sessions values (1401961260, 8880, 1, 'fffe93bb07863666d0e59550af88b8c97f95fa79', 0, '');
+
+insert into sessions values (1401615480, 2700, 1, '7f95fa79ff07863666d0e59550af88bfe93bb8c9', 0, '');
+insert into sessions values (1401789300, 4440, 1, '7f95fa79ff07863666d0e59550af88bfe93bb8c9', 0, '');
+insert into sessions values (1401826320, 2580, 0, '7f95fa79ff07863666d0e59550af88bfe93bb8c9', 0, '');
+insert into sessions values (1401959400, 3900, 1, '7f95fa79ff07863666d0e59550af88bfe93bb8c9', 0, '');
+insert into sessions values (1401997020, 840, 0, '7f95fa79ff07863666d0e59550af88bfe93bb8c9', 0, '');
+insert into sessions values (1402050060, 5820, 1, '7f95fa79ff07863666d0e59550af88bfe93bb8c9', 0, '');
+insert into sessions values (1402059120, 1320, 1, '7f95fa79ff07863666d0e59550af88bfe93bb8c9', 0, '');
+
+#-------------------------------------------------------------------------------
+
+insert into sessions values (1402213440, 3060, 1, '8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+insert into sessions values (1402300860, 2160, 1, '8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+insert into sessions values (1402309860, 1320, 0, '8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+insert into sessions values (1402386540, 3660, 1, '8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+insert into sessions values (1402398960, 840, 1, '8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+insert into sessions values (1402556040, 2580, 0, '8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+insert into sessions values (1402647660, 4980, 1, '8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+insert into sessions values (1402671900, 11400, 1, '8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+insert into sessions values (1402773360, 8640, 1, '8bfe93bb895fa79fd0e59550af8c97ff07863666', 0, '');
+
+insert into sessions values (1402261260, 11700, 0, '95fa79ff07863666d0e59550af88bfe93bb8c97f', 0, '');
+insert into sessions values (1402522800, 9780, 0, '95fa79ff07863666d0e59550af88bfe93bb8c97f', 0, '');
+insert into sessions values (1402692960, 7020, 0, '95fa79ff07863666d0e59550af88bfe93bb8c97f', 0, '');
