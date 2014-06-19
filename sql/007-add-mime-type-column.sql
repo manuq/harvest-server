fbca33a04c75b22b1db4efbd07931589ed944685
Manuel Quiñones <manuel.por.aca@gmail.com>
2014-06-19 16:43:53 -0300

    Tablas simplificadas
    No se consideran instancias.  En cambio, launches se relaciona con
    activity directamente (relación una activity, muchos launches) y con
    learners directamente (relación un learner, muchos launches).
    
    También por simplicidad, se reducen los archivos SQL a uno solo, sin
    migraciones.

--- sql/007-add-mime-type-column.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE instances ADD COLUMN mime_type VARCHAR(255) AFTER keep;
--- sql/009-move-spents.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE instances DROP COLUMN spent_time;
-ALTER TABLE launches ADD COLUMN spent_time INT(11) AFTER timestamp;
