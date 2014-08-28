73a472718c1b0f60d9a86a49000d19ed9acdfa1e
Juan Manuel Pereira <jpereira@plan.ceibal.edu.uy>
2014-08-28 17:12:06 -0300

    Se arregla consulta de activites - e&m

--- harvest/monitor/sql/activities_monitoreo.sql
+++ harvest/monitor/sql/activities_monitoreo.sql
@@ -2,7 +2,7 @@ SELECT 'actividad','fecha','serial_number','duracion_sugar','uuid','fecha_act','
 'departamento','tipo_institucion', 'codigo_institucion', 'perfil','grado','fecha_nac','modelo_equipo'
 UNION ALL
 (select ifnull(a.activity_name, s.bundle_id), DATE(FROM_UNIXTIME(s.timestamp)) as fecha, s.serial_number, 
-truncate(SUM(s.spent_time)/60, 2) as duracion_sugar, t.*  duracion_sugar, t.* 
+truncate(SUM(s.spent_time)/60, 2) as duracion_sugar, t.* 
 from launches s, activities a, tilo t 
 where s.bundle_id = a.bundle_id and s.serial_number = t.serial_number 
 group by s.serial_number, DATE(FROM_UNIXTIME(s.timestamp)), s.bundle_id)
