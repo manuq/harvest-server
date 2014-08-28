ec87b71e4f3e2d355140a80c36564599f3b3d2cb
Juan Manuel Pereira <jpereira@plan.ceibal.edu.uy>
2014-08-28 17:02:16 -0300

    Arreglo consultas de e&m

--- harvest/monitor/sql/activities_monitoreo.sql
+++ harvest/monitor/sql/activities_monitoreo.sql
@@ -1,7 +1,8 @@
 SELECT 'actividad','fecha','serial_number','duracion_sugar','uuid','fecha_act','serial_number',
 'departamento','tipo_institucion', 'codigo_institucion', 'perfil','grado','fecha_nac','modelo_equipo'
 UNION ALL
-(select ifnull(a.activity_name, s.bundle_id), DATE(FROM_UNIXTIME(s.timestamp)) as fecha, s.serial_number, SUM(s.spent_time)/60 as duracion_sugar, t.* 
+(select ifnull(a.activity_name, s.bundle_id), DATE(FROM_UNIXTIME(s.timestamp)) as fecha, s.serial_number, 
+truncate(SUM(s.spent_time)/60, 2) as duracion_sugar, t.*  duracion_sugar, t.* 
 from launches s, activities a, tilo t 
 where s.bundle_id = a.bundle_id and s.serial_number = t.serial_number 
 group by s.serial_number, DATE(FROM_UNIXTIME(s.timestamp)), s.bundle_id)
--- harvest/monitor/sql/sugar_gnome_monitoreo.sql
+++ harvest/monitor/sql/sugar_gnome_monitoreo.sql
@@ -1,7 +1,8 @@
 SELECT 'fecha', 'is_sugar', 'duracion', 'uuid', 'fecha_act', 'serial_number', 'departamento',
 'tipo_institucion', 'codigo_institucion', 'perfil', 'grado', 'fecha_nac', 'modelo_equipo'
 UNION ALL 
-SELECT DATE(FROM_UNIXTIME(s.timestamp)) as fecha, s.is_sugar, SUM(s.spent_time)/60 as duracion, t.*
+SELECT DATE(FROM_UNIXTIME(s.timestamp)) as fecha, s.is_sugar, 
+truncate(SUM(s.spent_time)/60, 2) as duracion, t.*
 FROM sessions s
 JOIN tilo t ON s.serial_number = t.serial_number 
 GROUP BY s.serial_number, DATE(FROM_UNIXTIME(s.timestamp)), is_sugar
