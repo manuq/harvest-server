bca4b27d259eccd7baf7205e81f228aecfd2a605
Juan Manuel Pereira <jpereira@plan.ceibal.edu.uy>
2014-08-28 19:33:11 -0300

    Cambios en la consulta sesiones_monitoreo, se trunca la duracion para que solo aparezcan dos digitos

--- harvest/monitor/sql/sesiones_monitoreo.sql
+++ harvest/monitor/sql/sesiones_monitoreo.sql
@@ -1,6 +1,6 @@
 SELECT 'fecha','duracion','uuid','fecha_act','serial_number',
 'departamento','tipo_institucion', 'codigo_institucion', 'perfil','grado','fecha_nac','modelo_equipo'
 UNION ALL
-(select DATE(FROM_UNIXTIME(s.timestamp)), s.spent_time/60, t.* 
+(select DATE(FROM_UNIXTIME(s.timestamp)), truncate(s.spent_time/60, 2), t.* 
 from sessions s, tilo t 
 where t.serial_number = s.serial_number)
