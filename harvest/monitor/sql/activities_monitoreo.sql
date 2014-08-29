2898f595eb4ea6017bdcbaa6d8b91a9b6f31eb75
Juan Manuel Pereira <jpereira@plan.ceibal.edu.uy>
2014-08-29 12:07:21 -0300

    Pequeños cambios en las consultas de E&M

--- harvest/monitor/sql/activities_monitoreo.sql
+++ harvest/monitor/sql/activities_monitoreo.sql
@@ -1,7 +1,7 @@
-SELECT 'actividad','fecha','serial_number','duracion_sugar','uuid','fecha_act','serial_number',
+SELECT 'actividad','fecha','duracion_sugar','uuid','fecha_act','serial_number',
 'departamento','tipo_institucion', 'codigo_institucion', 'perfil','grado','fecha_nac','modelo_equipo'
 UNION ALL
-(select ifnull(a.activity_name, s.bundle_id), DATE(FROM_UNIXTIME(s.timestamp)) as fecha, s.serial_number, 
+(select ifnull(a.activity_name, s.bundle_id), DATE(FROM_UNIXTIME(s.timestamp)) as fecha,  
 truncate(SUM(s.spent_time)/60, 2) as duracion_sugar, t.* 
 from launches s, activities a, tilo t 
 where s.bundle_id = a.bundle_id and s.serial_number = t.serial_number 
--- harvest/monitor/sql/applications_monitoreo.sql
+++ harvest/monitor/sql/applications_monitoreo.sql
@@ -1,7 +1,7 @@
-SELECT 'aplicacion','fecha','serial_number','duracion_gnome','uuid','fecha_act','serial_number',
+SELECT 'aplicacion','fecha','duracion_gnome','uuid','fecha_act','serial_number',
 'departamento','tipo_institucion', 'codigo_institucion', 'perfil','grado','fecha_nac','modelo_equipo'
 UNION ALL
-(select ifnull(a.app_id, s.app_name), DATE(FROM_UNIXTIME(s.timestamp)) as fecha, s.serial_number, truncate(SUM(s.spent_time)/60, 2) as duracion_gnome, t.*
+(select ifnull(a.app_id, s.app_name), DATE(FROM_UNIXTIME(s.timestamp)) as fecha, truncate(SUM(s.spent_time)/60, 2) as duracion_gnome, t.*
 from gnome_launches s, gnome_alias a, tilo t
 where s.app_name = a.app_name and s.serial_number = t.serial_number
 group by s.serial_number, DATE(FROM_UNIXTIME(s.timestamp)), s.app_name)
