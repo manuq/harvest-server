906201e0bed28a59c2c3222e362be4098b227994
Esteban Arias <earias@plan.ceibal.edu.uy>
2014-11-05 14:06:25 -0200

    optimiza consulta sessiones monitoreo

--- harvest/monitor/sql/sesiones_monitoreo.sql
+++ harvest/monitor/sql/sesiones_monitoreo.sql
@@ -1,9 +1,4 @@
-SELECT 'fecha','duracion','uuid','fecha_act','serial_number',
-'departamento','tipo_institucion', 'codigo_institucion', 'perfil','grado','fecha_nac','modelo_equipo'
-UNION ALL
-(select DATE(FROM_UNIXTIME(s.timestamp)), truncate(s.spent_time/60, 2), t.* 
+select DATE(FROM_UNIXTIME(s.timestamp)) as fecha, truncate(s.spent_time/60, 2) as duracion, t.serial_number as serial_number, t.departamento as departamento, t.tipo_institucion as tipo_institucion, t.codigo_institucion as codigo_institucion, t.perfil as perfil, t.grado as grado, t.fecha_nac as fecha_nac, t.modelo_equipo as modelo_equipo
 from sessions s, tilo t 
 where t.serial_number = s.serial_number
 and codigo_institucion in (select codigo_institucion from muestras where id_muestra = 2)
-)
-
